e9b3fefd084e55f2f58ab46e7f58d40d3e2a6af8
Maximus-SSU <maxim-firsov777@mail.ru>
2024-03-03 18:34:48 +0400

    modified:   img_links.txt 	renamed:    scraper/requirements.txt -> requirements.txt 	modified:   scraper/linkScraper.py

--- scraper/linkScraper.py
+++ scraper/linkScraper.py
@@ -8,68 +8,60 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) #убрать предупреждение  InsecureRequestWarning
 
 # Список всех ссылок на  нужные категории
-LINK_DOMEN_UDOCHKI="https://саратов.хищник.рф/catalog/rybalka/udilishcha/?PAGEN_1="
-LINK_DOMEN_KATUSHKI="https://саратов.хищник.рф/catalog/rybalka/katushki/?PAGEN_1="
-LINK_DOMEN_LESKA="https://саратов.хищник.рф/catalog/rybalka/leska-shnury/?PAGEN_1="
-LINK_DOMEN_KOROBKI="https://саратов.хищник.рф/catalog/rybalka/korobki-i-tubusy/korobki/?PAGEN_1="
-LINK_DOMEN_YASCHIKI="https://саратов.хищник.рф/catalog/rybalka/korobki-i-tubusy/yashchiki/?PAGEN_1="
-LINK_DOMEN_NODZI="https://саратов.хищник.рф/catalog/turizm/orudiya-i-instrumenty/nozhi/?PAGEN_1="
-LINK_DOMEN_MULTITOOLS="https://саратов.хищник.рф/catalog/turizm/orudiya-i-instrumenty/instrument/?PAGEN_1="
-LINK_DOMEN_MOTORY="https://саратов.хищник.рф/catalog/lodki-i-motory/lodochnye-motory/lodochnye-motory-dvs/?PAGEN_1="
-LINK_DOMEN_LODKI="https://саратов.хищник.рф/catalog/lodki-i-motory/naduvnye-lodki-pvkh/?PAGEN_1="
-LINK_DOMEN_PALATKI1="https://саратов.хищник.рф/catalog/turizm/palatki-i-pologi/palatki-kempingovye/"
-LINK_DOMEN_PALATKI2="https://саратов.хищник.рф/catalog/turizm/palatki-i-pologi/palatki-turisticheskie/"
-LINK_DOMEN_STULYA="https://саратов.хищник.рф/catalog/turizm/turisticheskaya-mebel/stulya/?PAGEN_1="
-LINK_DOMEN_STOLI="https://саратов.хищник.рф/catalog/turizm/turisticheskaya-mebel/stoly/"
-LINK_DOMEN_KURTKI="https://саратов.хищник.рф/catalog/odezhda-i-obuv/demisezonnaya-odezhda/kurtki/"
-LINK_MODEL_BOTINKI="https://саратов.хищник.рф/catalog/odezhda-i-obuv/obuv/botinki/?PAGEN_1="
-LINK_MODEL_SAPOGI1="https://саратов.хищник.рф/catalog/odezhda-i-obuv/obuv/sapogi-zimnie/"
-LINK_MODEL_SAPOGI2="https://саратов.хищник.рф/catalog/odezhda-i-obuv/obuv/sapogi-rezinovye/"
-LINK_MODEL_SAPOGI3="https://саратов.хищник.рф/catalog/odezhda-i-obuv/obuv/sapogi-zabrodnye/"
-LINK_MODEL_FOOTBOLKI="https://саратов.хищник.рф/catalog/odezhda-i-obuv/letnyaya-odezhda/futbolki/?PAGEN_1="
-
+LINK_CATEGORY_INSTRUMENT_SOKI="https://online.metro-cc.ru/category/bezalkogolnye-napitki/soki-morsy-nektary"
 # Основная ссылка на сайт
+# LINK_PRODUCT_DOMEN="https://leroymerlin.ru/product/"
+# LINK_UPLOAD_DOMEN="https://cdn.leroymerlin.ru/lmru/image/upload/"
 MAIN_LINK_DOMEN="https://саратов.хищник.рф"
+
 
 # #получаем готовый Url необходимой страницы для BS4, параметр verify можно отключить, в зависимости от сайта
 def getNewUrl(newUrl):
-    r=requests.get(newUrl,verify=False)
-    return r.text
-
+    r=requests.get(newUrl)
+    if r.status_code==200:
+        print(f"\nSUCCESS - Status CODE: = {r.status_code}")
+        return r.text
+    else:
+        print(f"\nERROR - Status CODE: = {r.status_code}")
+        return r.text
 
 
 
 # парсим каждую страницу
 def ParsePage(soup, item_group):
-    soup=soup.find('div', class_="catalog_block")
-    rawLinks=soup.findAll('a') #Список всех картинок
-    for rawLink in rawLinks:
-        rawLink_link= rawLink.get('href') 
-        picture_link= rawLink.find('img')
-        if picture_link is not None:
+    soup=soup.find('div', class_="p135dg85_plp")
+    if soup is not None:
+        rawLinks=soup.findAll('div',class_="p155f0re_plp largeCard") #Список всех картинок
+        for rawLink in rawLinks:
+            rawLink=rawLink.find('a')
+            rawLink_link= rawLink.get('href') 
+            pictureLink_title=rawLink.get('aria-label')
+            picture_link= rawLink.find('source', media_= "(min-width: 1200px)")
+            # picture_link= picture_link_soup.get('srcset')
+            if picture_link is not None:
 
-            # debug MODE:
-            #####################################################################
-            # count+=1
-            # print("\n________________________________________________________")
-            # print("\n********************************************************")
-            # print(count)
-            # print("\n********************************************************")
-            # print(rawLink)
-            # print("\n********************************************************")
-            # print(picture_link)
-            # print("\n********************************************************")
-             #####################################################################
-            pictureLink_title= picture_link.get('title')
-            pictureLink_upload_raw=picture_link.get('src')
-            pictureLink_upload=MAIN_LINK_DOMEN+pictureLink_upload_raw
-            preparedlink=MAIN_LINK_DOMEN+rawLink_link
-            preparedLinks.append([item_group,pictureLink_title,preparedlink,pictureLink_upload])
-    # rawLinks_length=len(rawLinks)# количество всех ссылок
-        # if rawLinks_length==21 :
-        #     return True
-        # else :
-        #     return False
+                # debug MODE:
+                #####################################################################
+                # count+=1
+                print("\n________________________________________________________")
+                # print("\n********************************************************")
+                # print(count)
+                print("\n********************************************************")
+                print(rawLink)
+                print("\n********************************************************")
+                print(picture_link)
+                print("\n********************************************************")
+                #####################################################################
+                pictureLink_upload=picture_link.get('srcset')
+                preparedlink=MAIN_LINK_DOMEN+rawLink_link
+                preparedLinks.append([item_group,pictureLink_title,preparedlink,pictureLink_upload])
+        # rawLinks_length=len(rawLinks)# количество всех ссылок
+            # if rawLinks_length==21 :
+            #     return True
+            # else :
+            #     return False
+    else:
+        print("\nERR: Soup is NONE\n")
 
 
 
@@ -78,13 +70,13 @@ def Parse(startUrl,item_group,max_pages_in_group):
     if max_pages_in_group>0:
         for page_count in range (1,max_pages_in_group+1):
             url=startUrl+str(page_count)
-            # print("\nTRY SCRAPPING : "+ url+"\n")
+            print("\nTRY SCRAPPING : "+ url+"\n")
             Soup=BeautifulSoup(getNewUrl(url),'lxml')
             ParsePage(Soup,item_group)
             page_count+=1
     else:
         url=startUrl
-        # print("\nTRY SCRAPPING : "+ url+"\n")
+        print("\nTRY SCRAPPING : "+ url+"\n")
         Soup=BeautifulSoup(getNewUrl(url),'lxml')
         ParsePage(Soup,item_group)
 
@@ -103,26 +95,7 @@ def CreateLinkDocument(ListOut):
 
 preparedLinks=[]
 start_DATA=[
-    (LINK_DOMEN_UDOCHKI,"Удилища",10),
-    (LINK_DOMEN_KATUSHKI,"Катушки",10),
-    (LINK_DOMEN_LESKA,"Леска",5),
-    (LINK_DOMEN_KOROBKI,"Коробки",5),
-    (LINK_DOMEN_YASCHIKI,"Ящики", 2),
-    (LINK_DOMEN_NODZI,"Ножи", 5),
-    (LINK_DOMEN_MULTITOOLS,"Мультитулы", 3),
-    (LINK_DOMEN_MOTORY,"Моторы",2),
-    (LINK_DOMEN_LODKI,"Лодки",2),
-    (LINK_DOMEN_PALATKI1,"Палатки",0),
-    (LINK_DOMEN_PALATKI2,"Палатки",0),
-    (LINK_DOMEN_STULYA,"Стулья",2),
-    (LINK_DOMEN_STOLI,"Столы",0),
-    (LINK_DOMEN_KURTKI,"Куртки",0),
-    (LINK_MODEL_BOTINKI,"Ботинки",2),
-    (LINK_MODEL_SAPOGI1,"Сапоги",0),
-    (LINK_MODEL_SAPOGI2,"Сапоги",0),
-    (LINK_MODEL_SAPOGI3,"Сапоги",0),
-    (LINK_MODEL_FOOTBOLKI,"Футболки",2)
-    ]
+    (LINK_CATEGORY_INSTRUMENT_SHURUPOVERTS,"Шуруповерт",0)]
 
 k=len(start_DATA)
 for i in tqdm(range(0,k)):
